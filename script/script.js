3b0d07d94ed54ac05f335033b7609d3e7adfef95
Nima Khamisipour <Nimkam52@gmail.com>
2023-10-07 17:19:34 +0200

    filesyst
    checking if fetch api works

--- script/script.js
+++ script/script.js
@@ -1,40 +1,92 @@
 
-//Modal
-const openButtons = document.querySelectorAll("[data-open-modal]");
-const closeButtons = document.querySelectorAll("[data-close-modal]");
-const modals = document.querySelectorAll("[data-modal]");
+// Modal
+const openButtons = document.querySelectorAll("[data-open-modal]")
+const closeButtons = document.querySelectorAll("[data-close-modal]")
+const modals = document.querySelectorAll("[data-modal]")
 
 openButtons.forEach((button, index) => {
   button.addEventListener("click", () => {
-    modals[index].showModal();
-  });
-});
+    modals[index].showModal() // For each modal open when clicked
+  })
+})
 
 closeButtons.forEach((button, index) => {
   button.addEventListener("click", () => {
-    modals[index].close();
-  });
-});
+    modals[index].close() // For each button in modal close when clicked
+  })
+})
 
 modals.forEach(modal => {
   modal.addEventListener("click", e => {
-    const dialogDimensions = modal.getBoundingClientRect();
+    let dialogDimensions = modal.getBoundingClientRect()
     if (
       e.clientX < dialogDimensions.left ||
       e.clientX > dialogDimensions.right ||
       e.clientY < dialogDimensions.top ||
       e.clientY > dialogDimensions.bottom
     ) {
-      modal.close();
+      modal.close() // For each modal close when clicked outside of given dimensions
     }
-  });
-});
+  })
+})
+
+// Clone template function
+function cloneTemplate() {
+  // Clone html template tag
+  const template = document.getElementById("article-template")
+  const clone = document.importNode(template.content, true)
+
+  // Append to the "output" id
+  const outputElement = document.getElementById("output")
+  outputElement.appendChild(clone)
+}
 
 
+// Get data from .txt | fetch only works on github
+const articlesData = []
+fetch("../filesystem.txt")
+    .then(response => response.text())
+    .then(data => {
+        let lines = data.split("\n")
+        let headers = lines[0].split(" | ")
 
-/*❗
-  Create logic to read filesystem.txt file to get content 
-*/
-/*❗
-  Create logic to put content inside of id output in same format as example article
-*/
+        for (let i = 1; i < lines.length; i++) {
+            let values = lines[i].split(" | ")
+            let articleData = {}
+
+            for (let j = 0; j < headers.length; j++) {
+                articleData[headers[j]] = values[j].replace(/(^"|"$)/g, "")  // Remove quotes
+            }
+
+            articlesData.push(articleData)
+
+            // For each item in articlesData clone template and populate at that index | Fetch is asynchronous 
+            cloneTemplate()
+            populateTemplateAtIndex(i - 1)
+
+            console.log(articlesData)
+        }
+    })
+    .catch(error => {
+        console.log("Error:", error)
+    })
+
+// Populate with data at index x
+function populateTemplateAtIndex(index) {
+  let article = document.querySelectorAll(".article")[index]
+  
+  // If Index is 2nd and every 11th index therafter then add class span2 => 2, 13 , 24, etc
+  if ((index === 1) || ((index - 1) % 11 === 0 && index !== 0)) {
+    article.classList.add("span2")  
+  }
+
+  // Add information into the data attributes
+  if (article && articlesData[index]) {
+      let data = articlesData[index]
+      article.querySelector("[data-modal-title]").textContent = data["data-modal-title"]
+      article.querySelector("[data-modal-author-date]").textContent = data["data-modal-author-date"]
+      article.querySelector("[data-modal-img]").src = data["data-modal-img"]
+      article.querySelector("[data-modal-img-caption]").textContent = data["data-modal-img-caption"]
+      article.querySelector("[data-modal-content]").textContent = data["data-modal-content"]
+  }
+}
